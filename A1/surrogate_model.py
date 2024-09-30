e678fae193f0389d93e989490c92d8dbd2eb8ca0
laurensschink <163992516+laurensschink@users.noreply.github.com>
2024-09-30 17:17:19 +0200

    Update surrogate_model.py
    Added code to train a first random forest

--- A1/surrogate_model.py
+++ A1/surrogate_model.py
@@ -22,8 +22,17 @@ class SurrogateModel:
 
         :param df: the dataframe with performances
         :return: Does not return anything, but stores the trained model in self.model
+
+        LJS: added first start. I would think we need to use the train_test_split to do a small parameter search to optimize the model. 
+        But i'm not sure.
         """
-        raise NotImplementedError()
+        self.df = df 
+        features = df.columns[:-1]
+        label = df.columns[-1]
+        X_train, X_test, y_train, y_test = train_test_split(df[features],df[label],test_size=0.33)
+        self.model.fit(X_train,y_train)
+        y_pred = self.model.predict(X_test)
+        print('mse:', mean_squared_error(y_pred,y_test),'r2:', r2_score(y_test,y_pred))
 
     def predict(self, theta_new):
         """

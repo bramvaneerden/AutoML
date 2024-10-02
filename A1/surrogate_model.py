71e71aab922c7a177983b3aca3d650c032e5a0e4
BramvE <bramveerden@gmail.com>
2024-10-02 15:30:56 +0200

    gridsearch RF

--- A1/surrogate_model.py
+++ A1/surrogate_model.py
@@ -5,6 +5,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.pipeline import Pipeline
+from sklearn.model_selection import RandomizedSearchCV
+
 import pandas as pd
 
 
@@ -13,7 +15,7 @@ class SurrogateModel:
     def __init__(self, config_space):
         self.config_space = config_space
         self.df = None
-        self.model = None
+        self.model = RandomForestRegressor()
 
     def fit(self, df):
         """
@@ -27,12 +29,42 @@ class SurrogateModel:
         But i'm not sure.
         """
         self.df = df 
-        features = df.columns[:-1]
-        label = df.columns[-1]
-        X_train, X_test, y_train, y_test = train_test_split(df[features],df[label],test_size=0.33)
-        self.model.fit(X_train,y_train)
+        encoded = pd.get_dummies(df)
+
+        features = encoded.columns[:-1]
+        label = encoded.columns[-1]
+
+        X_train, X_test, y_train, y_test = train_test_split(encoded[features],encoded[label],test_size=0.33)
+
+        param_dist = {
+            'n_estimators': [100, 200, 500],
+            'max_depth': [None, 10, 20, 50],
+            'min_samples_split': [2, 5, 10],
+            'min_samples_leaf': [1, 2, 4],
+            'max_features': ['auto', 'sqrt', 'log2'],
+            'bootstrap': [True, False]
+        }
+
+        rf_random = RandomizedSearchCV(estimator=self.model, param_distributions=param_dist,
+                               n_iter=100, cv=3, verbose=2, random_state=42, n_jobs=-1)
+
+        rf_random.fit(X_train,y_train)
+        print("best params: " , rf_random.best_params_ , "\n best score: " , rf_random.best_score_)
+        # print("cv results: ", rf_random.cv_results_)
+
+        self.model = rf_random.best_estimator_
         y_pred = self.model.predict(X_test)
         print('mse:', mean_squared_error(y_pred,y_test),'r2:', r2_score(y_test,y_pred))
+
+
+
+
+        # features = df.columns[:-1]
+        # label = df.columns[-1]
+        # X_train, X_test, y_train, y_test = train_test_split(df[features],df[label],test_size=0.33)
+        # self.model.fit(X_train,y_train)
+        # y_pred = self.model.predict(X_test)
+        # print('mse:', mean_squared_error(y_pred,y_test),'r2:', r2_score(y_test,y_pred))
 
     def predict(self, theta_new):
         """
